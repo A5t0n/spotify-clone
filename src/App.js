5b65899d3ad21baaf8f002bcca508c0766c151b0
Aston Varghese <astonvarghese8@gmail.com>
2025-02-14 14:19:06 -0500

    created a basic login page with spotify logo and a button. Added basic styling. Also created a new file called spotify.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="app">
-      <h1>Heyyyy I am Aston</h1>
-
       {/* Spotify Logo */}
-
       {/* Login with Spotify Button */}
+
+      <Login />
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,2 @@
+//no caps on the file name as it is not a component
+//https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
