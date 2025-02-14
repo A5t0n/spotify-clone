b7df6abecad643e4ebd47704b99279f2eed067f5
Aston Varghese <astonvarghese8@gmail.com>
2025-02-14 14:20:18 -0500

    added authpoint to let spotify api to authorize

--- src/spotify.js
+++ src/spotify.js
@@ -1,2 +1,4 @@
 //no caps on the file name as it is not a component
 //https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
