a6ba3ae7a77dcf50f3d75ef97b06a7862d33845a
Aston Varghese <astonvarghese8@gmail.com>
2025-02-26 14:08:03 -0500

    boilerplate body and sidebar for player

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Player.css'
+import Sidebar from './Sidebar'
+import Body from './Body'
 
 function Player ({spotify}) {
     return (
-        <div>
-            {/* Sidebar */}
-            {/* Body */}
+        <div className='player'>
+            <div className='player_body'>
+                {/* Sidebar */}
+                <Sidebar />
+                {/* Body */}
+                <Body/>
+            </div>
+            
             {/* Footer */} 
         </div>
     )
