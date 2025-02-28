392fbad9d1df8e0492a1181b02c004aed4dd7f41
Aston Varghese <astonvarghese8@gmail.com>
2025-02-28 14:18:07 -0500

    added icons into sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 function Sidebar() {
     return (
@@ -10,9 +13,11 @@ function Sidebar() {
             src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'
             alt='logo'/>
 
-            <SidebarOption title='Home'/>
-            <SidebarOption title='Search'/>
-            <SidebarOption title='Your Library'/>
+            <SidebarOption Icon={HomeIcon} title='Home'/>
+            <SidebarOption Icon={SearchIcon} title='Search'/>
+            <SidebarOption Icon={LibraryMusicIcon} title='Your Library'/>
+            <strong className='sidebar_title'>PLAYLISTS</strong>
+            <hr/>
        </div>
     )
 }
